171eef35ee14345fa85579b2066c7e3f2331d332
NorVirae <norbertmbafrank@gmail.com>
2023-08-17 16:04:58 +0100

    hook up lambda functions to playfab

--- GameVoucher.cs
+++ GameVoucher.cs
@@ -1,5 +1,11 @@
-﻿using Amazon.Lambda.APIGatewayEvents;
+﻿using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
+using Amazon.Runtime;
+using Amazon.Runtime.Internal.Transform;
 using Newtonsoft.Json;
 using System.Diagnostics;
 using System.Text;
@@ -23,22 +29,83 @@ namespace AwsGameVoucherSystem
 
             //var bodyContent = request.IsBase64Encoded ? Convert.FromBase64String(request.Body) : Encoding.UTF8.GetBytes(request.Body);
             //using var memStream = new MemoryStream(bodyContent);
-
-          
-            if (request.Body != null)
+            var dbRegion = Environment.GetEnvironmentVariable("DB_REGION");
+            var dbClient = new AmazonDynamoDBClient(RegionEndpoint.GetBySystemName(dbRegion));
+            try
             {
-                var bodyContent = JsonConvert.DeserializeObject<VoucherRequest>(request.Body);
+                if (request.Body != null)
+                {
+                    var bodyContent = JsonConvert.DeserializeObject<Vouchers>(request.Body);
+                    var dbContext = new DynamoDBContext(dbClient);
+
+                    string IdAndVoucherCode = Guid.NewGuid().ToString();
+                    var voucher = new Voucher()
+                    {
+                        Email = bodyContent.Email,
+                        Id = IdAndVoucherCode,
+                        VoucherGoldQuantity = bodyContent.GoldQuantity,
+                        VoucherCode = IdAndVoucherCode,
+
+                        CreationDate = DateTime.Now,
+                        Expiry = DateTime.Now.AddDays(20)
+                    };
+                    await dbContext.SaveAsync<Voucher>(voucher);
+
+
+                    response.Body = JsonConvert.SerializeObject(new { Email = bodyContent.Email, VoucherCode = IdAndVoucherCode });
+
+                }
             }
-
-
-
-            
+            catch (Exception ex)
+            {
+                response.Body = JsonConvert.SerializeObject(new { Error = ex.Message });
+            }
 
             return response;
         }
         public async Task<APIGatewayProxyResponse> ConsumeVoucher(APIGatewayProxyRequest request)
         {
+            var response = new APIGatewayProxyResponse()
+            {
+                Headers = new Dictionary<string, string>(),
+                StatusCode = 200
+            };
 
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+            response.Headers.Add("Access-Control-Allow-Headers", "*");
+            response.Headers.Add("Access-Control-Allow-Methods", "OPTIONS, post");
+            response.Headers.Add("Content-Type", "application/json");
+
+            //var bodyContent = request.IsBase64Encoded ? Convert.FromBase64String(request.Body) : Encoding.UTF8.GetBytes(request.Body);
+            //using var memStream = new MemoryStream(bodyContent);
+            var dbRegion = Environment.GetEnvironmentVariable("DB_REGION");
+            var dbClient = new AmazonDynamoDBClient(RegionEndpoint.GetBySystemName(dbRegion));
+            try
+            {
+                if (request.Body != null)
+                {
+                    var bodyContent = JsonConvert.DeserializeObject<ConsumeVoucherRequest>(request.Body);
+
+                    var key = new Dictionary<string, AttributeValue> {
+                        ["VoucherCode"] = new AttributeValue { S = bodyContent.VoucherCode }
+                    };
+                    var reqs = new GetItemRequest
+                    {
+                        Key = key,
+                        TableName = tableName,
+                    };
+                    await dbClient.GetItemAsync<Voucher>("Voucher", key, new CancellationToken { });
+
+                    response.Body = JsonConvert.SerializeObject(new { Email = bodyContent.Email, VoucherCode = IdAndVoucherCode });
+
+                }
+            }
+            catch (Exception ex)
+            {
+                response.Body = JsonConvert.SerializeObject(new { Error = ex.Message });
+            }
+
+            return response;
         }
 
         private void GenerateAndStoreVouchers(int count)
--- Vouchers.cs
+++ Vouchers.cs
@@ -8,12 +8,25 @@ using System.Threading.Tasks;
 namespace AwsGameVoucherSystem
 {
     [DynamoDBTable("Vouchers")]
-    public class VoucherRequest
+    public class Vouchers
+    {
+        public string Email;
+        public int GoldQuantity;
+    }
+
+    public class Voucher
     {
         public string Email;
         public string Id;
         public string VoucherCode;
+        public int VoucherGoldQuantity;
         public DateTime CreationDate;
         public DateTime Expiry;
     }
+    public class ConsumeVoucherRequest
+    {
+        public string Email;
+        public string VoucherCode;
+    }
+
 }
