bfb55603d8ce7c88a96f9a34d01ea1f3ccd7aea1
NorVirae <norbertmbafrank@gmail.com>
2023-08-17 12:15:29 +0100

    initial premade combo

---
+++ GameVoucher.cs
@@ -0,0 +1,50 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Text;
+
+namespace AwsGameVoucherSystem
+{
+    public class GameVoucher
+    {
+        public async Task<APIGatewayProxyResponse> PurchaseVoucher(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            var response = new APIGatewayProxyResponse()
+            {
+                Headers = new Dictionary<string, string>(),
+                StatusCode = 200
+            };
+
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+            response.Headers.Add("Access-Control-Allow-Headers", "*");
+            response.Headers.Add("Access-Control-Allow-Methods", "OPTIONS, post");
+            response.Headers.Add("Content-Type", "application/json");
+
+            //var bodyContent = request.IsBase64Encoded ? Convert.FromBase64String(request.Body) : Encoding.UTF8.GetBytes(request.Body);
+            //using var memStream = new MemoryStream(bodyContent);
+
+          
+            if (request.Body != null)
+            {
+                var bodyContent = JsonConvert.DeserializeObject<VoucherRequest>(request.Body);
+            }
+
+
+
+            
+
+            return response;
+        }
+        public async Task<APIGatewayProxyResponse> ConsumeVoucher(APIGatewayProxyRequest request)
+        {
+
+        }
+
+        private void GenerateAndStoreVouchers(int count)
+        {
+
+        }
+
+    }
+}
---
+++ VoucherRequest.cs
@@ -0,0 +1,19 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwsGameVoucherSystem
+{
+    [DynamoDBTable("Vouchers")]
+    public class VoucherRequest
+    {
+        public string Email;
+        public string Id;
+        public string VoucherCode;
+        public DateTime CreationDate;
+        public DateTime Expiry;
+    }
+}
